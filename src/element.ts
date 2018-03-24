cac078ba1553b7c90c98b495ef2ddea35a583093
Thomas Milotti <thomas.milotti@gmail.com>
2018-03-24 17:35:59 +0100

    Effacement de fichiers initials plus utils

--- src/element.ts
+++
@@ -1,7 +0,0 @@
-export class Element {
-    option: ElementOption;
-
-}
-
-export type ElementType =
-    LiteralElementOption<LiteralType, LiteralInString> | ListOption<Element> | ObjectOption<Element, any>;
--- src/help.ts
+++
@@ -1,9 +0,0 @@
-export function isArray(value: any) {
-    return typeof value === 'object' && value !== null && typeof value !== 'function' && isLength(value.length);
-}
-
-
-function isLength(value: any) {
-    return typeof value === 'number' &&
-        value > -1 && value % 1 === 0 && value <= Number.MAX_SAFE_INTEGER;
-}
